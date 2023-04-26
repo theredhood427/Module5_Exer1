ecdc26d8a2bd630b22b070e4531e4f9e2408e1c2
theredhood427 <bangsil.ronrusselle@auf.edu.ph>
2023-04-26 16:28:35 +0800

    initial commit

---
+++ Module5/Module5/Exercise1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Exercise1 : ContentPage
+    {
+        public Exercise1()
+        {
+            InitializeComponent();
+        }
+        private void OnGenderToggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+            {
+                // Switch is turned on, do something
+                var selectedGender = "Male";
+
+                if (!genderSwitch.IsToggled)
+                {
+                    selectedGender = "Female";
+                }
+
+                // Do something with the selected gender
+            }
+        }
+    }
+}
---
+++ Module5/Module5/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Exercise1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Exercise1());
+        }
+    }
+}
